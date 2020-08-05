cb7b3c706a6bf35888923195cf64d43b14ea882d
Enhex <enhex0@gmail.com>
2020-08-05 05:59:44 +0300

    use type alias

--- src/layout/base.h
+++ src/layout/base.h
@@ -8,18 +8,20 @@ namespace layout
 	template <typename element_derived>
 	struct base
 	{
+		using element_t = element<element_derived>;
+
 		virtual ~base() = default;
 
 		virtual std::type_info const& type_info() const = 0;
 
 		// owning element which uses this layout for its children
-		element<element_derived>* parent = nullptr;
+		element_t* parent = nullptr;
 
 		virtual void fit() = 0;
 		virtual void lay() = 0;
 		virtual void perform() = 0;
 
-		void layout_start_recurse(element<element_derived>& element)
+		void layout_start_recurse(element_t& element)
 		{
 				element.pre_layout();
 				for(auto& child : element.children) {
