ae34892c72b72643544e2fd6d2f0e347d864f597
Enhex <enhex0@gmail.com>
2021-06-17 16:24:54 +0300

    prevent clicks that do nothing from propagating to the modal and closing the menu

--- src/gui/file_dialog_modal.cpp
+++ src/gui/file_dialog_modal.cpp
@@ -4,4 +4,8 @@ file_dialog_modal::file_dialog_modal() :
 	file_di(create_child<file_dialog>())
 {
 	file_di.visibility_element = this;
+
+	context->input_manager.mouse_press.subscribe(&file_di, [](int key, int mods) {
+		// prevent clicks on the dialog from propagating to the modal
+	});
 }
