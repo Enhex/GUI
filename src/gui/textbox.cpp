2b3f7baaf7773a4858dbf4848a36ab63a5e402f3
Enhex <enhex0@gmail.com>
2022-05-04 20:05:28 +0300

    deduplicate code

--- src/gui/textbox.cpp
+++ src/gui/textbox.cpp
@@ -1,65 +1,8 @@
 #include "textbox.h"
 
-#include "text.h"
-
 textbox::textbox()
 {
-	// apply text's style by default
-	style = text::element_name;
-	apply_style();
-	// and then textbox specific style
 	style = element_name;
-	apply_style();
-}
-
-void textbox::setup(int new_font, float new_font_size, std::string const& new_str)
-{
-	setup(context->vg, new_font, new_font_size, new_str);
-}
-
-void textbox::setup(NVGcontext* vg, int new_font, float new_font_size, std::string const& new_str)
-{
-	font = new_font;
-	font_size = new_font_size;
-	set_text(new_str);
-}
-
-void textbox::set_text(std::string const& new_str)
-{
-	str = new_str;
-	update_bounds();
-}
-
-void textbox::set_style(style::style_t const& style)
-{
-	auto read = [&](auto& property, std::string&& name)
-	{
-		using property_t = std::remove_reference_t<decltype(property)>;
-
-		auto iter = style.find(name);
-		if (iter != style.end()) {
-			property = std::any_cast<property_t>(iter->second);
-			return true;
-		}
-		return false;
-	};
-
-	bool bounds_need_update = false;
-
-	std::string font_name;
-	if (read(font_name, "font")) {
-		font = context->style_manager.font_name_to_id(font_name);
-		bounds_need_update = true;
-	}
-
-	if(read(font_size, "font_size")) {
-		bounds_need_update = true;
-	}
-
-	if(bounds_need_update)
-		update_bounds();
-
-	read(color, "color");
 }
 
 void textbox::update_bounds()
@@ -91,13 +34,6 @@ void textbox::update_bounds(NVGcontext* vg)
 	}
 
 	nvgRestore(vg);
-}
-
-void textbox::init_font(NVGcontext* vg)
-{
-	nvgFontSize(vg, font_size);
-	nvgFontFaceId(vg, font);
-	//nvgTextAlign(vg, NVG_ALIGN_LEFT | NVG_ALIGN_BASELINE);
 }
 
 void textbox::draw(NVGcontext* vg)
