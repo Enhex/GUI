ed9566864855f8963fdec831ac9df5a5bc247a5e
Enhex <enhex0@gmail.com>
2022-07-15 18:41:26 +0300

    add release build to build_all script

--- build_all.sh
+++ build_all.sh
@@ -1,1 +1,8 @@
-py build.py && cd ./example/ && py build.py && cd ..
+#!/bin/bash
+
+if [ "$1"="r" ];
+then
+	py build.py -r && cd ./example/ && py build.py -r && cd ..
+else
+	py build.py && cd ./example/ && py build.py && cd ..
+fi
